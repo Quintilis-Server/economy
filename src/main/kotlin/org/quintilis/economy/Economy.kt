1e635003f38bd5993eb7db969bba73f641af9163
abacate <rafael.schrott02@gmail.com>
2025-10-17 10:16:45 -0300

    initial commit

---
+++ src/main/kotlin/org/quintilis/economy/Economy.kt
@@ -0,0 +1,14 @@
+package org.quintilis.economy
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Economy : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
